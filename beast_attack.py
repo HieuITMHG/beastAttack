c24b7aa74405b40ab75c3a19dee21aa842fe0ad1
TuLe <tuleaibk@gmail.com>
2025-04-01 10:16:43 +0700

    code xong beast

--- beast_attack.py
+++ beast_attack.py
@@ -23,13 +23,92 @@ class Brower:
     
     def send_data_to_server(self, data:bytes):
         ciphers = AES_CBC_encrypt(data, self.key, self.iv)
-        iv = ciphers[-BLOCK_SIZE]
+        self.iv = ciphers[-BLOCK_SIZE:]
         return ciphers
 
+def find_data_len(victim:Brower):
+    return len(victim.data)
+
 def attack(victim:Brower)->bytes:
-    pass
+
+    data_len = 0
+    known_data = b""
+
+    # Xac dinh kich thuoc du lieu
+    data_len = find_data_len(victim)
+
+    # tao 1 request de lay iv
+    r = victim.send_data_to_server(b"kjsfhjshfshkdf")
+    iv = r[-16:]
+
+
+    while(len(known_data) != data_len):
+        # 
+        index = len(known_data) # byte can tim tiep theo
+        block_id = index//16 #byte can tim o block thu may (tinh tu 0)
+        
+        inject_size = 15 - (index%16)
+        inject = inject_size * b'X'
+
+        # tao request de lay cipher mau(cua data dung)
+        r1 = victim.send_data_to_server(inject + victim.data)
+
+        # lay cipher mau (ti nua so sanh)
+        # cipher cua block_id
+        sample_cipher = r1[16*block_id: 16*block_id+16]
+
+        # prev_cipher la cipher xor voi plaintext block_id
+
+        prev_cipher = None
+        if block_id == 0:
+            prev_cipher = iv
+        else:
+            # cipher text block_id -1
+            start = (block_id -1)*16
+            end = start + 16
+            print("prev cipher", start, end)
+            prev_cipher = r1[start:end]
+
+        # update iv
+        iv = r1[-16:]
+        # print(iv)
+        # print(type(iv))
+        # debug
+        print('------------------------------------------')
+        print ("know: ", known_data)
+        print("index:", index)
+        print("block id:", block_id)
+        print("inject size: ", inject_size)
+        print("inject value: ", inject)
+        print('------------------------------------------')
+        for guess in [int.to_bytes(x) for x in range(256)]:
+            p = None
+            if(len(known_data) >= 15):
+                p = known_data[-15:] + guess
+            else:
+                p = b'X'*(15 - len(known_data))  + known_data + guess
+            
+            p = XOR(p, iv)
+            p = XOR(p, prev_cipher)
+
+            # tao request de so sanh
+            r2 = victim.send_data_to_server(p + victim.data)
+            iv = r2[-16:]
+
+            # so sanh
+            if r2[0:16] == sample_cipher:
+                print("Tim duoc byte thu", len(known_data),":", guess)
+                
+                known_data += guess
+                print("Biet duoc:", known_data)
+                break # dung vong
+            if(guess == int.to_bytes(255)):
+                raise RuntimeError(":)))")
+    return known_data
 
 if __name__ == "__main__":
-    victim = Brower(b"this data is secret")
+    # victim = Brower(b"this data is secret hacker can not see")
+    victim = Brower(request)
     result = attack(victim)
     print("Attack result: {}".format(result))
+

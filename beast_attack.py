82273529e9b455b2fb874b258a92672252c3e742
TuLe <tuleaibk@gmail.com>
2025-04-01 08:31:24 +0700

    update

--- beast_attack.py
+++ beast_attack.py
@@ -9,6 +9,27 @@ SESSION_KEY = ''.join(random.choices(string.ascii_letters + string.digits, k=BLO
 request = b"""GET /dashboard HTTP/1.1\nHost: example.com\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64)\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9\nCookie: SESSIONID=SECRET1234\nConnection: close\n"""
 start_iv = ''.join(random.choices(string.ascii_letters + string.digits, k=BLOCK_SIZE)).encode('utf-8')
 
+class Brower:
+    def __init__(self, data:bytes, key:bytes|None = None, iv:bytes|None=None):
+        if key is None:
+            key = bytes([random.randint(0, 255) for _ in range(16)])
+            
+        if iv is None:
+            iv =  key = bytes([random.randint(0, 255) for _ in range(16)])
+
+        self.key = key
+        self.iv = iv
+        self.data = data
+    
+    def send_data_to_server(self, data:bytes):
+        ciphers = AES_CBC_encrypt(data, self.key, self.iv)
+        iv = ciphers[-BLOCK_SIZE]
+        return ciphers
+
+def attack(victim:Brower)->bytes:
+    pass
 
 if __name__ == "__main__":
-    print(AES_CBC_encrypt(plain_text=request, start_iv=start_iv, key=SESSION_KEY))
+    victim = Brower(b"this data is secret")
+    result = attack(victim)
+    print("Attack result: {}".format(result))
